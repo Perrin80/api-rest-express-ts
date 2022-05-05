7c7cb617defe3205cf2233cc51b624d834e97cef
Perrin80 <c.gonzalez@hotmail.cl>
2022-05-05 12:08:33 -0400

    seven commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ import tokenValidator from '../middlewares/tokenValidator'
 const apiRoutes = Router()
 
 apiRoutes.use('/', healthRoutes)
-apiRoutes.use('/task', tokenValidator(), taskRoutes)
+apiRoutes.use('/tasks', tokenValidator(), taskRoutes)
 apiRoutes.use('/auth', authRoutes)
 
 export default apiRoutes
